68f25c0eeaae350381603f2c8f5e9b5e9fb14f9c
Mark Nevin <mnevin1@my.wctc.edu>
2025-10-06 15:57:37 -0500

    Add character - input name, description

--- Program.cs
+++ Program.cs
@@ -35,6 +35,11 @@ do
             Id = marios.Count == 0 ? 1 : marios.Max(c => c.Id) + 1
         };
 
+        Console.WriteLine("Enter Name:");
+        mario.Name = Console.ReadLine();
+        Console.WriteLine("Enter Description:");
+        mario.Description = Console.ReadLine();
+
   }
     else if (choice == "3")
     {
