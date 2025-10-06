dc91c577dba51e1bafca0ca6b6e5bae4b688d658
Mark Nevin <mnevin1@my.wctc.edu>
2025-10-06 15:40:34 -0500

    Deserialize mario json from file into List<Mario>

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿using NLog;
+using System.ComponentModel.Design.Serialization;
+using System.Text.Json;
+
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
 
 logger.Info("Program started");
 
-Console.WriteLine("Hello, World!");
+string marioFileName = "mario.json";
+List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(marioFileName))!;
 
 logger.Info("Program ended");
