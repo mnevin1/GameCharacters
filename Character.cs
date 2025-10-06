48cb73190f6e462f6fad0f90d91d6a92856c5fff
Mark Nevin <mnevin1@my.wctc.edu>
2025-10-06 15:30:18 -0500

    Define base class

---
+++ Character.cs
@@ -0,0 +1,13 @@
+public abstract class Character
+{
+    public UInt64 Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual string Display()
+    {
+        return $"ID: {Id}\nName: {Name}\nDescription: {Description}\n";
+    }
+}
