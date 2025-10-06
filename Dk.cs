007a6ec923305da6bf35659eb61c3c220cd0de22
Mark Nevin <mnevin1@my.wctc.edu>
2025-10-06 17:43:06 -0500

    create classes for dk and sf2

---
+++ Dk.cs
@@ -0,0 +1,8 @@
+public class Dk : Character
+{
+    public string? Species { get; set; }
+    public override string Display()
+    {
+        return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nSpecies: {Species}\n";
+    }
+}
